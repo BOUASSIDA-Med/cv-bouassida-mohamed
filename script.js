fb9f6353e6c6ec810868e648933e16443e129fe2
BOUASSIDA-Med <72794772+BOUASSIDA-Med@users.noreply.github.com>
2021-03-18 13:09:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// NAVBAR
+let lastScrollTop = 0;
+navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function(){
+  const scrollTop = window.pageTOffset || this.document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    navbar.style.top="-50px";
+  } else {
+    navbar.style.top="0";
+  }
+  lastScrollTop = scrollTop;
+
+});
+
+// TYPED
+var typed = new Typed('.typed', {
+  strings: ["Bonjour à tous et à toutes, je me présente, je m'appelle Mohamed BOUASSIDA", "Je suis actuellement un Master 2 Compétences Complémentaires en Informatique à l’Université Claude Bernard Lyon 1." ,"Je suis à la recherche d’un stage en développement web, pour une durée de six mois."],
+  typeSpeed: 50,
+});
+  
+// COMPTEUR LIVE
+let compteur = 0;
+
+$(window).scroll(function() {
+
+  const top = $('.counter').offset().top - window.innerHeight;
+
+  if (compteur == 0 && $(window).scrollTop() > top) {
+    $('.counter-value').each(function() {
+      let $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+        countNum : countTo
+      },
+      {
+        duration: 10000,
+        easing: 'swing',
+        step: function() {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $this.text(this.countNum);
+        }
+      });
+    });
+    compteur = 1;
+  }
+});
+
+//AOS
+AOS.init();
