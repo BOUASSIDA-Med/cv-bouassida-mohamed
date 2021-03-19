f0601b0502aca21c760d8990b948613cc6146968
BOUASSIDA-Med <72794772+BOUASSIDA-Med@users.noreply.github.com>
2021-03-19 03:15:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ window.addEventListener('scroll', function(){
 
 // TYPED
 var typed = new Typed('.typed', {
-  strings: ["Bonjour à tous et à toutes, je me présente, je m'appelle Mohamed BOUASSIDA", "Je suis actuellement un Master 2 Compétences Complémentaires en Informatique à l’Université Claude Bernard Lyon 1." ,"Je suis à la recherche d’un stage en développement web, pour une durée de six mois."],
+  strings: ["Bonjour à tous et à toutes, je me présente, je m'appelle Mohamed BOUASSIDA", "Je suis actuellement un Master 2 Compétences Complémentaires en Informatique à l’Université Claude Bernard Lyon 1." ,"Je suis à la recherche d’un stage en informatique, pour une durée de six mois."],
   typeSpeed: 50,
 });
   
